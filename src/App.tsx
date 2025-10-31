2565d4ab6b792e0a8599e55a3ba0f04cdecc3bea
vr1jorg <holachepi@gmail.com>
2025-10-31 16:47:57 -0500

    installing tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="p-4 flex flex-col gap-4">
+      <h1 className='text-xl font-bold'>Occupass Dashboard</h1>
+      <Actions />
+    </main>
+  )
+}
+
+function Actions() {
+  function handleListOrders() {
+    console.log("Fetching orders")
+  }
+
+  return (
+    <ul>
+      <button className='cursor-pointer bg-green-200 animate-pulse hover:animate-none hover:bg-gray-200 rounded-md outline px-2 py-3 inline' onClick={handleListOrders}>Click here to fetch the latest orders</button>
+    </ul>
   )
 }
 
